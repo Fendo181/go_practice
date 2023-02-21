412b73a31d72c6b68ff551fe69f4b59ccc24fc79
Fendo181 <fendo181git@gmail.com>
2023-02-21 15:35:41 +0900

    別の書き方でgoのfor文の例を追加した

--- A_Tour_of_Go/control/for.go
+++ A_Tour_of_Go/control/for.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	sum := 0
-	for i := 0; i < 10; i++ {
+	var sum,i int
+	sum = 0
+	for i = 0; i < 10; i++ {
 		sum += i
 	}
+	sum = sum + 10
 	fmt.Println(sum)
 }
